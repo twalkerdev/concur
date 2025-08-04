154baa4c8782cb60fb98b272584925c2fb9e8613
twalkerdev <tyler.walker.dev@outlook.com>
2025-08-04 03:22:08 -0400

    adds css and typings to component

--- packages/concurui/src/lib/concurui.tsx
+++ packages/concurui/src/lib/concurui.tsx
@@ -1,7 +1,19 @@
+import * as styles from '../styles/main.css'
+
 export function Concurui() {
   return (
     <div className={styles['container']}>
       <h1>Welcome to Concurui!</h1>
+      <div className="space-y-4">
+        <div className="w-96 bg-white shadow rounded">w-96</div>
+        <div className="w-80 bg-white shadow rounded">w-80</div>
+        <div className="w-72 bg-white shadow rounded">w-72</div>
+        <div className="w-64 bg-white shadow rounded">w-64</div>
+        <div className="w-60 bg-white shadow rounded">w-60</div>
+        <div className="w-56 bg-white shadow rounded">w-56</div>
+        <div className="w-52 bg-white shadow rounded">w-52</div>
+        <div className="w-48 bg-white shadow rounded">w-48</div>
+      </div>
     </div>
   )
 }
