3a3da6daa6290ed681f9a79ae851a127652c8c5b
twalkerdev <tyler.walker.dev@outlook.com>
2025-08-04 03:55:09 -0400

    Adds Storybook

--- packages/concurui/tailwind.config.js
+++ packages/concurui/tailwind.config.js
@@ -4,7 +4,7 @@ const { join } = require('path')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    join(__dirname, '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'),
+    join(__dirname, '{src,pages,components,app}/**/*!(*.storybook|*.stories|*.spec).{ts,tsx,html}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
